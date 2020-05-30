ecfe8b7facbafdf3c4c66db77d7a83aa7ea3092f
Sergey Romanov <nashe.dobrov@gmail.com>
2020-05-30 18:47:29 +0300

    Finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,7 +1,9 @@
 class Dom {
   constructor(selector) {
     this.$el =
-      typeof selector === 'string' ? document.querySelector(selector) : selector
+      typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector
   }
   html(html) {
     if (typeof html === 'string') {
@@ -33,6 +35,26 @@ class Dom {
       this.$el.appendChild(node)
     }
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
   }
 }
 
